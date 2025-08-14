0004a1963a97c82fc837fa44162e474b69584eaa
Dan Victor Lofranco <danvictorprogramming1@gmail.com>
2025-08-15 04:48:19 +0800

    feat: added test on notes module

--- src/api/notes/notes.controller.spec.ts
+++ src/api/notes/notes.controller.spec.ts
@@ -1,20 +1,108 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { NotesController } from './notes.controller';
 import { NotesService } from './notes.service';
+import { JwtResponse } from '../authentication/passport-strategies/jwt/jwt.strategy';
+import { PrismaModule } from 'src/db/prisma/prisma.module';
+import { PrismaService } from 'src/db/prisma/prisma.service';
+import { prismaServiceMock as db } from 'src/test/prisma.service.mock';
 
 describe('NotesController', () => {
   let controller: NotesController;
+  let service: NotesService;
 
+  const mockNotesService = {
+    create: jest.fn(),
+    findAllByUser: jest.fn(),
+    findOne: jest.fn(),
+    update: jest.fn(),
+    remove: jest.fn(),
+  };
+
+  const user: JwtResponse = { id: '123', email: 'email@example.com' };
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [NotesController],
-      providers: [NotesService],
+      providers: [
+        {
+          provide: NotesService,
+          useValue: mockNotesService,
+        },
+      ],
+      imports: [
+        {
+          module: PrismaModule,
+          providers: [{ provide: PrismaService, useValue: db }],
+          exports: [PrismaService],
+        },
+      ],
     }).compile();
 
     controller = module.get<NotesController>(NotesController);
+    service = module.get<NotesService>(NotesService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('create', () => {
+    it('should call NotesService.create with correct parameters', async () => {
+      const createNoteDto = { title: 'Test Note', content: 'Test Content' };
+      mockNotesService.create.mockResolvedValue({ id: '1', ...createNoteDto });
+
+      const result = await controller.create(createNoteDto, { user });
+      expect(service.create).toHaveBeenCalledWith(createNoteDto, user);
+      expect(result).toEqual({ id: '1', ...createNoteDto });
+    });
+  });
+
+  describe('findAllByUser', () => {
+    it('should call NotesService.findAllByUser with correct parameters', async () => {
+      const notes = [{ id: '1', title: 'Note 1' }];
+      mockNotesService.findAllByUser.mockResolvedValue(notes);
+
+      const result = await controller.findAllByUser({ user });
+      expect(service.findAllByUser).toHaveBeenCalledWith(user);
+      expect(result).toEqual(notes);
+    });
+  });
+
+  describe('findOne', () => {
+    it('should call NotesService.findOne with correct parameters', async () => {
+      const id = '1';
+      const note = { id: '1', title: 'Note 1' };
+      mockNotesService.findOne.mockResolvedValue(note);
+
+      const result = await controller.findOne(id, { user });
+      expect(service.findOne).toHaveBeenCalledWith(id, user);
+      expect(result).toEqual(note);
+    });
+  });
+
+  describe('update', () => {
+    it('should call NotesService.update with correct parameters', async () => {
+      const id = '1';
+      const updateNoteDto = {
+        title: 'Updated Note',
+        content: 'Updated Content',
+      };
+      const updatedNote = { id: '1', ...updateNoteDto };
+      mockNotesService.update.mockResolvedValue(updatedNote);
+
+      const result = await controller.update(id, updateNoteDto, { user });
+      expect(service.update).toHaveBeenCalledWith(id, updateNoteDto, user);
+      expect(result).toEqual(updatedNote);
+    });
+  });
+
+  describe('remove', () => {
+    it('should call NotesService.remove with correct parameters', async () => {
+      const id = '1';
+      mockNotesService.remove.mockResolvedValue({ id: '1' });
+
+      const result = await controller.remove(id, { user });
+      expect(service.remove).toHaveBeenCalledWith(id, user);
+      expect(result).toEqual({ id: '1' });
+    });
   });
 });
--- src/api/notes/notes.service.spec.ts
+++ src/api/notes/notes.service.spec.ts
@@ -1,12 +1,65 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { NotesService } from './notes.service';
+import { PrismaModule } from 'src/db/prisma/prisma.module';
+import { PrismaService } from 'src/db/prisma/prisma.service';
+import { prismaServiceMock as db } from 'src/test/prisma.service.mock';
+import { Note, User } from '@prisma/client';
+import { JwtResponse } from '../authentication/passport-strategies/jwt/jwt.strategy';
 
 describe('NotesService', () => {
   let service: NotesService;
 
+  const sampleData: Note = {
+    id: '1',
+    title: 'Sample Note',
+    content: 'This is a sample note content.',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    userId: 'user-123',
+    categoryId: null,
+  };
+  const sampleDatas: Note[] = [
+    {
+      id: '1',
+      title: 'Sample Note',
+      content: 'This is a sample note content.',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      userId: 'user-123',
+      categoryId: null,
+    },
+    {
+      id: 'r',
+      title: 'Sample Note',
+      content: 'This is a sample note content.',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      userId: 'user-124',
+      categoryId: null,
+    },
+  ];
+  const sampleJwtUser: JwtResponse = {
+    id: 'user-123',
+    email: 'user@example.com',
+  };
+  const sampleUser: User = {
+    id: 'user-123',
+    email: 'user@example.com',
+    name: 'Test User',
+    role: 'USER',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  };
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [NotesService],
+      imports: [
+        {
+          module: PrismaModule,
+          providers: [{ provide: PrismaService, useValue: db }],
+          exports: [PrismaService],
+        },
+      ],
     }).compile();
 
     service = module.get<NotesService>(NotesService);
@@ -14,5 +67,64 @@ describe('NotesService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+  it('create() - create should create a note', async () => {
+    db.note.create.mockResolvedValue(sampleData);
+    const result = await service.create(
+      {
+        title: sampleData.title,
+        content: sampleData.content,
+      },
+      sampleJwtUser,
+    );
+    expect(result).toEqual(sampleData);
+  });
+  it('findAll() - findAll should return all notes', async () => {
+    db.note.findMany.mockResolvedValue([sampleData]);
+    const result = await service.findAll();
+    expect(result).toEqual([sampleData]);
+  });
+  it('findAllByUser() - findAllByUser should return all notes based on user', async () => {
+    db.note.findMany.mockResolvedValue([sampleData]);
+    const result = await service.findAllByUser(sampleJwtUser);
+    expect(db.note.findMany).toHaveBeenCalledWith({
+      where: { userId: sampleJwtUser.id },
+    });
+    expect(result).toEqual([sampleData]);
+  });
+  it('findOne() - findOne should return a note by ID)', async () => {
+    db.note.findFirst.mockResolvedValue(sampleData);
+    db.user.findUnique.mockResolvedValue(sampleUser);
+
+    const result = await service.findOne(sampleData.id, sampleJwtUser);
+    expect(db.note.findFirst).toHaveBeenCalledWith({
+      where: { id: sampleData.id, userId: sampleJwtUser.id },
+    });
+    expect(result).toEqual(sampleData);
+  });
+  it('update() - update should update a note', async () => {
+    db.note.findFirst.mockResolvedValue(sampleData);
+    db.note.update.mockResolvedValue(sampleData);
+    const result = await service.update(
+      sampleData.id,
+      { title: 'Updated Note', content: 'Updated content' },
+      sampleJwtUser,
+    );
+    expect(db.note.update).toHaveBeenCalledWith({
+      where: { id: sampleData.id },
+      data: { title: 'Updated Note', content: 'Updated content' },
+    });
+    expect(result).toEqual(sampleData);
+  });
+  it('update() - update should add categoryId if provided', async () => {});
+  it('update() - update should add tags if provided', async () => {});
+  it('remove() - remove should delete a note', async () => {
+    db.note.findFirst.mockResolvedValue(sampleData);
+    db.note.delete.mockResolvedValue(sampleData);
+    const result = await service.remove(sampleData.id, sampleJwtUser);
+    expect(db.note.delete).toHaveBeenCalledWith({
+      where: { id: sampleData.id },
+    });
+    expect(result).toEqual(sampleData);
   });
 });
