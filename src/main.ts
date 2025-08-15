b17617b12b7186e8c7a5010824795e6496fed1b5
Dan Victor Lofranco <danvictorprogramming1@gmail.com>
2025-08-15 11:50:19 +0800

    feat: added helmet for security

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import morgan from 'morgan';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -9,6 +10,7 @@ async function bootstrap() {
   });
 
   app.use(morgan('combined'));
+  app.use(helmet());
   const config = new DocumentBuilder()
     .setTitle('Zeniark Simple Note-Taking API')
     .setDescription('A simple note-taking API built with NestJS.')
